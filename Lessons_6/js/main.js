e3546c22cb98e51ba830f0576c74447af0aabd16
Egor <Twinkstriker@gmail.com>
2019-06-20 15:35:38 +0300

    Lesson_6

---
+++ Lessons_6/js/main.js
@@ -0,0 +1,22 @@
+let button_start = document.querySelector('.start'),
+	budget = document.querySelector('.budget-value'),
+	level = document.querySelector('.level-value'),
+	expenses = document.querySelector('.expenses-value'),
+	optionalexpenses = document.querySelector('.optionalexpenses-value'),
+	income = document.querySelector('.income-value'),
+	monthsavings = document.querySelector('.monthsavings-value'),
+	budyearsavingsget = document.querySelector('.budyearsavingsget-value'),
+	expenses_item = document.querySelector('.expenses-item'),
+	button_confirm_expenses = document.getElementsByTagName('button')[0],
+	button_confirm_optionalexpenses = document.getElementsByTagName('button')[1],
+	button_calc_day_budget = document.getElementsByTagName('button')[2],
+	choose_income = document.querySelector('#income'),
+	savings = document.querySelector('#savings'),
+	sum = document.querySelector('#sum'),
+	percent = document.querySelector('#percent'),
+	year = document.querySelector('.year-value'),
+	month = document.querySelector('.month-value'),
+	day = document.querySelector('.day-value');
+
+
+	
